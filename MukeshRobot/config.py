ae8e0776fa00a9054dc51f0721fe8391d1e66865
alone-hermit <133490910+alone-hermit@users.noreply.github.com>
2023-09-14 21:47:24 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,24 +1,25 @@
+import os
 
 class Config(object):
     LOGGER = True
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = os.environ['API_ID'] # integer value, dont use ""
+    API_HASH = os.environ['API_HASH']
+    TOKEN = os.environ['TOKEN']  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = int(os.environ['OWNER_ID']) # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
-    START_IMG = ""
-    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    SUPPORT_CHAT = os.environ['SUPPORT_CHAT']  # Your own group for support, do not add the @
+    START_IMG = os.environ['START_IMG']
+    EVENT_LOGS = int(os.environ['EVENT_LOGS'])  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    MONGO_DB_URI= os.environ['MONGO_DB_URI']
     # RECOMMENDED
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = os.environ['DATABASE_URL']  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        os.environ['CASH_API_KEY']  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = os.environ['TIME_API_KEY']
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
